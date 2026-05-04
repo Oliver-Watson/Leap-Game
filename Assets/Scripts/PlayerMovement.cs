1c61f1d776688192d1e7c92098adfad6e9df1e28
Oliver-Watson <owatson1313@gmail.com>
2026-05-04 16:15:09 +0100

    Change orientation based on mouse input

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,13 +1,25 @@
+using System;
+using Unity.Mathematics;
+using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
+using UnityEngine.Windows;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float jumpForce = 5f;
+    public float jumpForce = 10f;
     public float speed = 5f;
     public float groundDisplacement = 0.1f;
 
+    [SerializeField] private float sensitivity;
+    [SerializeField] private Transform orientation;
+    private float mouseX;
+
     private Vector2 moveAction;
+    private Vector2 mouseInput;
+    private Vector3 moveDirection;
     private Rigidbody rb;
 
     [SerializeField] private Transform groundCheck;
@@ -15,67 +27,58 @@ public class PlayerMovement : MonoBehaviour
     public float groundDistance = 1f;
 
     private bool isGrounded = false;
-    private bool jumpIntent;
-    private bool hasJumped = false;
+    public static bool hasJumped = false;
+    public static bool jumpedOffGround = false;
     private bool wasGrounded = false;
 
-    private float coyoteTime = 2f;
-    private float coyoteTimeCounter;
+    public static float coyoteTime = 4f;
+    public static float coyoteTimeCounter;
 
-    private float jumpBuffer = 2.2f;
+    private float jumpBuffer = 0.2f;
     private float jumpBufferCounter;
+
+    private float min = -1f;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        //Cursor.lockState = CursorLockMode.Locked;
+        //Cursor.visible = false;
     }
 
     private void Update()
     {
-        isGrounded = IsGrounded();
+        HandleMoveInput();
+        //HandleMouseInput();
 
-        Vector3 velocity = rb.linearVelocity;
-        velocity.x = moveAction.x * speed;
-        velocity.z = moveAction.y * speed;
-        rb.linearVelocity = velocity;
+        isGrounded = IsGrounded();
 
         if (isGrounded && !wasGrounded)
         {
             coyoteTimeCounter = coyoteTime;
 
             hasJumped = false;
+            jumpedOffGround = false;
         }
         else if (!isGrounded)
         {
             coyoteTimeCounter -= Time.deltaTime;
+            coyoteTimeCounter = Mathf.Max(coyoteTimeCounter, min);
         }
-
-        //if (jumpIntent)
-        //{
-        //    jumpBufferCounter = jumpBuffer;
-        //}
-        //else
-        //{
-        //    jumpBufferCounter -= Time.deltaTime;
-        //}
-        jumpBufferCounter -= Time.deltaTime;
 
         if (!hasJumped && coyoteTimeCounter > 0 && jumpBufferCounter > 0)
         {
             PerformJump();
             hasJumped = true;
+
+            if (isGrounded)
+            {
+                jumpedOffGround = true;
+            }
         }
 
-        //if (isGrounded && !wasGrounded)
-        //{
-        //    coyoteTimeCounter = coyoteTime;
-
-        //    hasJumped = false;
-        //}
-        //else if (!isGrounded)
-        //{
-        //    coyoteTimeCounter -= Time.deltaTime;
-        //}
+        jumpBufferCounter -= Time.deltaTime;
+        jumpBufferCounter = Mathf.Max(jumpBufferCounter, min);
 
         wasGrounded = isGrounded;
 
@@ -84,26 +87,68 @@ public class PlayerMovement : MonoBehaviour
         Debug.Log(hasJumped + "Has Jumped");
     }
 
-    private void Move(InputAction.CallbackContext context)
+    public void Move(InputAction.CallbackContext context)
     {
         moveAction = context.ReadValue<Vector2>();
         Debug.Log(moveAction + "Press");
     }
 
-    private void Jump(InputAction.CallbackContext context)
+    public void Jump(InputAction.CallbackContext context)
     {
         if (context.performed)
         {
-            //jumpIntent = true;
             jumpBufferCounter = jumpBuffer;
         }
-        //else
-        //{
-        //    jumpIntent = false;
-        //}
     }
 
-    void PerformJump()
+    //public void OnMouseMove(InputAction.CallbackContext context)
+    //{
+    //    mouseInput = context.ReadValue<Vector2>();
+    //}
+
+    //private void HandleMouseInput()
+    //{
+    //    mouseX += mouseInput.x * sensitivity * Time.deltaTime;
+
+    //    transform.forward = new Vector3(mouseX, 0f, 0f);
+
+    //    //transform.Rotate(transform.up, mouseX);
+
+    //    //transform.rotation = Quaternion.LookRotation(new Vector3(0, 0, 0), Camera.main.transform.up);
+
+    //    transform.rotation = Quaternion.Euler(0f, mouseX, 0f);
+
+
+    //    //transform.rotation *= Quaternion.Euler(0f, mouseInput.y * sensitivity * Time.deltaTime, 0f);
+
+    //    //pitch -= mouseInput.y * sensitivity * Time.deltaTime;
+    //    //pitch = Mathf.Clamp(pitch, -90f, 90f);
+
+
+    //    //transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, mouseX, 0f);
+
+    //    Debug.Log("Mouse input:" + mouseInput.x);
+    //}
+
+    private void HandleMoveInput()
+    {
+        moveDirection = orientation.forward * moveAction.y + orientation.right * moveAction.x;
+        //Vector3 velocity = rb.linearVelocity;
+        //velocity.x = moveAction.x * speed;
+        //velocity.z = moveAction.y * speed;
+        //rb.linearVelocity = velocity;
+        Vector3 velocity = rb.linearVelocity;
+        velocity.x = moveDirection.normalized.x * speed;
+        velocity.z = moveDirection.normalized.z * speed;
+        rb.linearVelocity = velocity;
+        Debug.Log("Move Direction: " + moveDirection.normalized);
+
+
+        //moveDirection = transform.forward * moveAction.y + transform.right * moveAction.x;
+        //rb.AddForce(moveDirection.normalized * speed, ForceMode.Force);
+    }
+
+    private void PerformJump()
     {
         rb.linearVelocity = new Vector3(rb.linearVelocity.x, jumpForce, rb.linearVelocity.z);
 
@@ -111,7 +156,7 @@ public class PlayerMovement : MonoBehaviour
         coyoteTimeCounter = 0f;
     }
 
-    private bool IsGrounded()
+    public bool IsGrounded()
     {
         isGrounded = Physics.Raycast(groundCheck.position, Vector3.down, groundDistance + groundDisplacement, groundMask);
 
