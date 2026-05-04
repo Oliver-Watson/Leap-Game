402049b04fd8c099a43d30285c02fbd039aaa2c6
Oliver-Watson <owatson1313@gmail.com>
2026-05-04 16:48:20 +0100

    Old commented code removed

--- Assets/Scripts/PlayerCamera.cs
+++ Assets/Scripts/PlayerCamera.cs
@@ -29,36 +29,12 @@ public class PlayerCamera : MonoBehaviour
 
     private void HandleMouseInput()
     {
-        //transform.Rotate(Vector3.up, mouseInput.x * sensitivity * Time.deltaTime);
-
-        //pitch -= mouseInput.y * sensitivity * Time.deltaTime;
-        //pitch = Mathf.Clamp(pitch, -90f, 90f);
-        //transform.localEulerAngles = new Vector3(pitch, transform.localEulerAngles.y, 0f);
-
-        ////Debug.Log("Mouse input:" + mouseInput.x);
-        ///
-
         rotationX += mouseInput.x * sensitivity * Time.deltaTime;
         rotationY -= mouseInput.y * sensitivity * Time.deltaTime;
         rotationY = Mathf.Clamp(rotationY, -90f, 90f);
 
-        transform.forward = new Vector3(rotationX, 0f, 0f);
-
-        //transform.Rotate(transform.up, mouseX);
-
-        //transform.rotation = Quaternion.LookRotation(new Vector3(0, 0, 0), Camera.main.transform.up);
-
         orientation.rotation = Quaternion.Euler(0f, rotationX, 0f);
         transform.rotation = Quaternion.Euler(rotationY, rotationX, 0f);
-
-
-        //transform.rotation *= Quaternion.Euler(0f, mouseInput.y * sensitivity * Time.deltaTime, 0f);
-
-        //pitch -= mouseInput.y * sensitivity * Time.deltaTime;
-        //pitch = Mathf.Clamp(pitch, -90f, 90f);
-
-
-        //transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, mouseX, 0f);
 
         Debug.Log("Mouse input: " + mouseInput.x);
     }
